69c7a0b49c7353581953ff33de79efb5f9dcfbea
Noé Marques <noeamarques@gmail.com>
2019-10-09 15:20:29 -0300

    first commit

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+
+  url = 'http://loine.com';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
