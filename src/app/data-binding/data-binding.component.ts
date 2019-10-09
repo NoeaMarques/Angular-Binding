51a042dde69212973f4486d9c65e20c1d0012f0c
Noé Marques <noeamarques@gmail.com>
2019-10-09 17:55:53 -0300

    bindings

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
 
   url = 'http://loine.com';
+  cursoAngular = true;
+  urlImagem = 'http://lorempixel.com/400/200/';
+
+  getValor() {
+
+    return 1;
+  }
+
+  getCurtirCurso() {
+
+    return true;
+
+  }
 
   constructor() { }
 
